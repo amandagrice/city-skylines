8f52078a5fa1fce070fd82e78e64ae88cd9a9648
Amanda Grice <algrice@bu.edu>
2020-03-05 21:49:21 -0500

    create blank mod

---
+++ SampleMod/BlankMod.cs
@@ -0,0 +1,20 @@
+﻿using ICities;
+using UnityEngine;
+
+namespace TemplateMod
+{
+
+    public class BlankMod : IUserMod
+    {
+
+        public string Name
+        {
+            get { return "Blank Mod"; }
+        }
+
+        public string Description
+        {
+            get { return "This mod does nothing. Just making sure things are visible. :D"; }
+        }
+    }
+}
