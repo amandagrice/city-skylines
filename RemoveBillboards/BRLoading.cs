c2d401e389e99885727b74ac9f8c85d5a0f6df79
Amanda Grice <algrice@bu.edu>
2020-03-16 20:22:25 -0400

    remove billboards

---
+++ RemoveBillboards/BRLoading.cs
@@ -0,0 +1,37 @@
+﻿using ColossalFramework.UI;
+using ICities;
+
+namespace BillboardRemover
+{
+    public class BRLoading : ILoadingExtension
+    {
+        public void OnCreated(ILoading loading){}
+
+        public void OnLevelLoaded(LoadMode mode) {
+            for (uint index = 0; index < PrefabCollection<BuildingInfo>.LoadedCount(); index++) {
+
+                BuildingInfo prefab = PrefabCollection<BuildingInfo>.GetLoaded(index);
+                if (prefab == null || prefab.m_props == null) continue;
+                for (int propIndex = 0; propIndex < prefab.m_props.Length; propIndex++) {
+                    BuildingInfo.Prop propItem = prefab.m_props[propIndex];
+                    if (propItem.m_prop == null) continue;
+
+                    string propName = propItem.m_prop.name;
+                    if (propName == "Billboard_big_variation"
+                        || propName == "Billboard_big_variation_01"
+                        || propName == "Billboard_big_variation_02"
+                        || propName == "Billboard_big_variation_04"
+                        || propName == "neon-yakisoba-noodles"
+                        || propName == "neon-burned-bean-coffee"
+                        || propName == "neon-morellos") {
+                        propItem.m_probability = 0;
+                    }
+                }
+            }
+        }
+
+        public void OnLevelUnloading() {}
+
+        public void OnReleased(){}
+    }
+}
---
+++ RemoveBillboards/BillboardRemover.cs
@@ -0,0 +1,10 @@
+﻿using ICities;
+
+namespace BillboardRemover
+{
+    public class BillboardRemoverMod : IUserMod
+    {
+        public string Name => "Billboard Remover";
+        public string Description => "Removes ugly billboards.";
+    }
+}
